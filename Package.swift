d5028abd664f4b128eabec23093628ad4cbf9814
feixue299 <ariablink299@gmail.com>
2023-05-29 14:43:50 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "KIRIEngineSDK",
             url: "https://repository.kiri-engine.com/repository/iOS-SDK/1.1.0/KIRIEngineSDK.xcframework.zip",
-            checksum: "dfe7384d7d5ef93482e07a2b15ea69094beacc56a896c276c1df38d461c68e09"
+            checksum: "77bc69b59c162f014053f1e64fe374d589f0c52c0e631738da40beeda8ade4de"
         )
     ]
 )
